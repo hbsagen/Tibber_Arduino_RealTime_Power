9f53842d815888db7eda624b1a65b805901fc4db
hbsagen <48381155+hbsagen@users.noreply.github.com>
2021-07-08 15:28:40 +0200

    Update tibber.ino
    Added "nc" to display if connection is lost for more than 10 sec.

--- tibber.ino
+++ tibber.ino
@@ -2,25 +2,25 @@
 #include <WiFiEspUdp.h>
 #include <TM1637TinyDisplay.h>
 
-char ssid[] = "insert_your_ssid";            // your network SSID (name)
-char pass[] = "insert_your_password";        // your network password
-int status = WL_IDLE_STATUS;     // the Wifi radio's status
-
-unsigned int localPort = 52522;  // local port to listen on
-int disptemp;
-
-char packetBuffer[64];          // buffer to hold incoming packet
+char ssid[] = "K40";                                     // your network SSID (name)
+char pass[] = "2eaee065047f63383b869ca7379bab26";        // your network password
+int status = WL_IDLE_STATUS;                             // the Wifi radio's status
+unsigned long nc;
+unsigned int localPort = 52522;                          // local port to listen on
+char packetBuffer[64];
 
 WiFiEspUDP Udp;
-TM1637TinyDisplay display(6, 7); //CLK, DIO
+TM1637TinyDisplay display(6, 7);                         //CLK, DIO
 
 void setup() {
   Serial.begin(115200);
   Serial3.begin(115200);
-  WiFi.init(&Serial3);
-
+  
   display.setBrightness(BRIGHT_1);
   display.clear();
+  display.showString("boot",4,0);
+  
+  WiFi.init(&Serial3);
 
   if (WiFi.status() == WL_NO_SHIELD) {
         while (true);
@@ -37,14 +37,20 @@ void loop() {
   int packetSize = Udp.parsePacket();
   
   if (packetSize) {
-    IPAddress remoteIp = Udp.remoteIP();
     int len = Udp.read(packetBuffer, 255);
     if (len > 0) {
       packetBuffer[len] = 0;
     }
-    
-    Serial.println(packetBuffer);
-    disptemp = atoi(packetBuffer);
-    display.showNumber(disptemp, false, 4, 0);    // Number, length=4, position=0 (left)
+
+    if (atoi(packetBuffer) > 9999){
+      display.showNumber(9999, false, 4, 0);
+    }
+    else {
+      display.showNumber(atoi(packetBuffer), false, 4, 0);
+      nc = millis();
+    }
+  }
+  else if (millis() - nc > 10000) {                      //time before timout, "nc" displayed
+      display.showString(" nc ",4,0);
   }
 }
